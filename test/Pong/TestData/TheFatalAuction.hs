b223bdcfed5732108c7894e683e27cf2fb740a98
laserpants <hildenjohannes@gmail.com>
2022-06-11 00:20:09 +0300

    Update tests

--- test/Pong/TestData/TheFatalAuction.hs
+++ test/Pong/TestData/TheFatalAuction.hs
@@ -51,32 +51,31 @@ program45 :: Program MonoType Ast
 program45 =
   Program
     ( Map.fromList
-        [ 
-          (
-            (Scheme (tUnit ~> tInt), "main")
-          , Function 
+        [
+          ( (Scheme (tUnit ~> tInt), "main")
+          , Function
               (fromList [(tUnit, "a")])
               ( tInt
-              , eLet 
+              , eLet
                   (tInt ~> tInt ~> tInt, "$var_foo_1")
                   (eVar (tInt ~> tInt ~> tInt, "$lam1"))
-                  (eCall 
-                    (tInt ~> tInt ~> tInt, "$var_foo_1")
-                    [eLit (PInt 4), eLit (PInt 5)]
+                  ( eCall
+                      (tInt ~> tInt ~> tInt, "$var_foo_1")
+                      [eLit (PInt 4), eLit (PInt 5)]
                   )
               )
           )
-        , (
-            (Scheme (tVar "a" ~> tVar "a" ~> tVar "a"), "foo")
-          ,  Function 
+        ,
+          ( (Scheme (tVar "a" ~> tVar "a" ~> tVar "a"), "foo")
+          , Function
               (fromList [(tVar 0, "x"), (tVar 0, "y")])
               ( tVar 0
               , eVar (tVar 0, "x")
               )
           )
-        , (
-            (Scheme (tInt ~> tInt ~> tInt), "$lam1")
-          ,  Function 
+        ,
+          ( (Scheme (tInt ~> tInt ~> tInt), "$lam1")
+          , Function
               (fromList [(tInt, "x"), (tInt, "y")])
               ( tInt
               , eVar (tInt, "x")
@@ -87,7 +86,7 @@ program45 =
 
 --  def foo(f : a -> a, y : b) : b=
 --    y
---  
+--
 --  def main(a : unit) : int =
 --    foo(lam(x) => x, 5)
 
@@ -95,40 +94,39 @@ program47 :: Program MonoType Ast
 program47 =
   Program
     ( Map.fromList
-        [ 
-          (
-            (Scheme (tUnit ~> tInt), "main")
-          , Function 
+        [
+          ( (Scheme (tUnit ~> tInt), "main")
+          , Function
               (fromList [(tUnit, "a")])
               ( tInt
-              , eLet 
+              , eLet
                   ((tVar 0 ~> tVar 0) ~> tInt ~> tInt, "$var_foo_1")
                   (eVar ((tVar 0 ~> tVar 0) ~> tInt ~> tInt, "$lam1"))
-                  (eCall 
-                    ((tVar 0 ~> tVar 0) ~> tInt ~> tInt, "$var_foo_1")
-                    [eVar (tVar 0 ~> tVar 0, "$lam2"), eLit (PInt 5)]
+                  ( eCall
+                      ((tVar 0 ~> tVar 0) ~> tInt ~> tInt, "$var_foo_1")
+                      [eVar (tVar 0 ~> tVar 0, "$lam2"), eLit (PInt 5)]
                   )
               )
           )
-        , (
-            (Scheme ((tVar "a" ~> tVar "a") ~> tVar "b" ~> tVar "b"), "foo")
-          ,  Function 
+        ,
+          ( (Scheme ((tVar "a" ~> tVar "a") ~> tVar "b" ~> tVar "b"), "foo")
+          , Function
               (fromList [(tVar 1 ~> tVar 1, "f"), (tVar 2, "y")])
               ( tVar 2
               , eVar (tVar 2, "y")
               )
           )
-        , (
-            (Scheme ((tVar "a0" ~> tVar "a0") ~> tInt ~> tInt), "$lam1")
-          ,  Function 
+        ,
+          ( (Scheme ((tVar "a0" ~> tVar "a0") ~> tInt ~> tInt), "$lam1")
+          , Function
               (fromList [(tVar 0 ~> tVar 0, "f"), (tInt, "y")])
               ( tInt
               , eVar (tInt, "y")
               )
           )
-        , (
-            (Scheme (tVar "a0" ~> tVar "a0"), "$lam2")
-          ,  Function 
+        ,
+          ( (Scheme (tVar "a0" ~> tVar "a0"), "$lam2")
+          , Function
               (fromList [(tVar 0, "x")])
               ( tVar 0
               , eVar (tVar 0, "x")
@@ -147,40 +145,39 @@ program49 :: Program MonoType Ast
 program49 =
   Program
     ( Map.fromList
-        [ 
-          (
-            (Scheme (tUnit ~> tInt), "main")
-          , Function 
+        [
+          ( (Scheme (tUnit ~> tInt), "main")
+          , Function
               (fromList [(tUnit, "a")])
               ( tInt
-              , eLet 
+              , eLet
                   ((tInt ~> tInt) ~> tInt ~> tInt, "$var_foo_1")
                   (eVar ((tInt ~> tInt) ~> tInt ~> tInt, "$lam1"))
-                  (eCall 
-                    ((tInt ~> tInt) ~> tInt ~> tInt, "$var_foo_1")
-                    [eVar (tInt ~> tInt, "$lam2"), eLit (PInt 5)]
+                  ( eCall
+                      ((tInt ~> tInt) ~> tInt ~> tInt, "$var_foo_1")
+                      [eVar (tInt ~> tInt, "$lam2"), eLit (PInt 5)]
                   )
               )
           )
-        , (
-            (Scheme ((tVar "a" ~> tVar "b") ~> tVar "a" ~> tVar "b"), "foo")
-          ,  Function 
+        ,
+          ( (Scheme ((tVar "a" ~> tVar "b") ~> tVar "a" ~> tVar "b"), "foo")
+          , Function
               (fromList [(tVar 0 ~> tVar 1, "f"), (tVar 0, "y")])
               ( tVar 1
               , eCall (tVar 0 ~> tVar 1, "f") [eVar (tVar 0, "y")]
               )
           )
-        , (
-            (Scheme ((tInt ~> tInt) ~> tInt ~> tInt), "$lam1")
-          ,  Function 
+        ,
+          ( (Scheme ((tInt ~> tInt) ~> tInt ~> tInt), "$lam1")
+          , Function
               (fromList [(tInt ~> tInt, "f"), (tInt, "y")])
               ( tInt
               , eCall (tInt ~> tInt, "f") [eVar (tInt, "y")]
               )
           )
-        , (
-            (Scheme (tInt ~> tInt), "$lam2")
-          ,  Function 
+        ,
+          ( (Scheme (tInt ~> tInt), "$lam2")
+          , Function
               (fromList [(tInt, "x")])
               ( tInt
               , eVar (tInt, "x")
