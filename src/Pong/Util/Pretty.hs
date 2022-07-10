bcaa9890b6b77d2099f7b58c7f402e054f399975
laserpants <hildenjohannes@gmail.com>
2022-07-10 21:12:47 +0300

    refactor

--- src/Pong/Util/Pretty.hs
+++ src/Pong/Util/Pretty.hs
@@ -6,31 +6,32 @@
 
 module Pong.Util.Pretty where
 
--- import Pong.Data
+import Pong.Data
 -- import Pong.Lang
--- import Pong.Util
--- import Prettyprinter
+import Pong.Util
+import Prettyprinter
+
 --
 -- parensIf :: Bool -> Doc a -> Doc a
 -- parensIf True doc = parens doc
 -- parensIf _ doc = doc
---
--- {- ORMOLU_DISABLE -}
---
--- instance Pretty Prim where
---  pretty =
---    \case
---      PBool   True  -> "true"
---      PBool   False -> "false"
---      PUnit         -> "()"
---      PInt    p     -> pretty p
---      PFloat  p     -> pretty p <> "f"
---      PDouble p     -> pretty p
---      PChar   p     -> squotes (pretty p)
---      PString p     -> dquotes (pretty p)
---
--- {- ORMOLU_ENABLE -}
---
+
+{- ORMOLU_DISABLE -}
+
+instance Pretty Prim where
+ pretty =
+   \case
+     PBool   True  -> "true"
+     PBool   False -> "false"
+     PUnit         -> "()"
+     PInt    p     -> pretty p
+     PFloat  p     -> pretty p <> "f"
+     PDouble p     -> pretty p
+     PChar   p     -> squotes (pretty p)
+     PString p     -> dquotes (pretty p)
+
+{- ORMOLU_ENABLE -}
+
 -- instance (Pretty v, Pretty (TypeVar v)) => Pretty (Row (Type v) v) where
 --  pretty =
 --    para
@@ -50,40 +51,45 @@ module Pong.Util.Pretty where
 --                        "," <+> doc
 --                   )
 --      )
---
--- newtype TypeVar a = TypeVar a
---
--- instance (Pretty (TypeVar Name)) where
---  pretty (TypeVar v) = pretty v
---
--- instance (Pretty (TypeVar Int)) where
---  pretty (TypeVar v) = "'" <> pretty v
---
--- instance (Pretty v, Pretty (TypeVar v)) => Pretty (Type v) where
---  pretty =
---    para
---      ( \case
---          TUnit -> "unit"
---          TBool -> "bool"
---          TInt -> "int"
---          TFloat -> "float"
---          TDouble -> "double"
---          TChar -> "char"
---          TString -> "string"
---          TVar v -> pretty (TypeVar v)
---          TRec row -> "{" <+> pretty row <+> "}"
---          TCon con ts ->
---            pretty con
---              <+> hsep (uncurry (parensIf . addParens) <$> ts)
---          TArr (t1, doc1) (_, doc2) ->
---            parensIf (isConT ArrT t1) doc1 <+> "->" <+> doc2
---      )
---    where
---      addParens =
---        cata
---          ( \case
---              TCon _ [] -> False
---              TCon{} -> True
---              TArr{} -> True
---              _ -> False
---          )
+
+newtype TypeVar a = TypeVar a
+
+instance (Pretty (TypeVar Name)) where
+  pretty (TypeVar v) = pretty v
+
+instance (Pretty (TypeVar Int)) where
+  pretty (TypeVar v) = "'" <> pretty v
+
+{- ORMOLU_DISABLE -}
+
+instance (Pretty v, Pretty (TypeVar v)) => Pretty (Type v) where
+  pretty =
+    para
+      ( \case
+          TUnit   -> "unit"
+          TBool   -> "bool"
+          TInt    -> "int"
+          TFloat  -> "float"
+          TDouble -> "double"
+          TChar   -> "char"
+          TString -> "string"
+          TVar v  -> pretty (TypeVar v)
+          --         TRec row -> "{" <+> pretty row <+> "}"
+          --         TCon con ts ->
+          --           pretty con
+          --             <+> hsep (uncurry (parensIf . addParens) <$> ts)
+          --         TArr (t1, doc1) (_, doc2) ->
+          --           parensIf (isConT ArrT t1) doc1 <+> "->" <+> doc2
+      )
+
+--   where
+--     addParens =
+--       cata
+--         ( \case
+--             TCon _ [] -> False
+--             TCon{} -> True
+--             TArr{} -> True
+--             _ -> False
+--         )
+
+{- ORMOLU_ENABLE -}
