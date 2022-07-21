acfe7900f80afc6527c7742e49369ca92ab5a745
laserpants <hildenjohannes@gmail.com>
2022-07-22 02:46:09 +0300

    refactor

--- teriyaki/app/Main.hs
+++ teriyaki/app/Main.hs
@@ -6,24 +6,24 @@ import Teriyaki.Lang
 import Teriyaki.Tree
 
 testConstructorEnv :: ConstructorEnv
-testConstructorEnv = constructorEnv
-    [
---      ("Some"     , ( ["Some", "None"], 1 ))
---    , ("None"     , ( ["Some", "None"], 0 ))
---    , ("Zero"     , ( ["Zero", "Succ"], 0 ))
---    , ("Succ"     , ( ["Zero", "Succ"], 1 ))
---    , ("Leaf"     , ( ["Leaf", "Node"], 0 ))
---    , ("Node"     , ( ["Leaf", "Node"], 3 ))
---    , ("Leaf'"    , ( ["Leaf'", "Node'"], 0 ))
---    , ("Node'"    , ( ["Leaf'", "Node'"], 5 ))
-      ("[]"       , ( ["[]", "(::)"], 0 ))
-    , ("(::)"     , ( ["[]", "(::)"], 2 ))
---    , ("(,)"      , ( ["(,)"], 2 ))
---    , ("Foo"      , ( ["Foo"], 2 ))
---    , ("#"        , ( ["#"], 1 ))
---    , ("{}"       , ( ["{}"], 0 ))
---    , ("Cons'"    , ( ["Nil'", "Cons'"], 3 ))
---    , ("Nil'"     , ( ["Nil'", "Cons'"], 0 ))
+testConstructorEnv =
+  constructorEnv
+    [ --      ("Some"     , ( ["Some", "None"], 1 ))
+      --    , ("None"     , ( ["Some", "None"], 0 ))
+      --    , ("Zero"     , ( ["Zero", "Succ"], 0 ))
+      --    , ("Succ"     , ( ["Zero", "Succ"], 1 ))
+      --    , ("Leaf"     , ( ["Leaf", "Node"], 0 ))
+      --    , ("Node"     , ( ["Leaf", "Node"], 3 ))
+      --    , ("Leaf'"    , ( ["Leaf'", "Node'"], 0 ))
+      --    , ("Node'"    , ( ["Leaf'", "Node'"], 5 ))
+      ("[]", (["[]", "(::)"], 0))
+    , ("(::)", (["[]", "(::)"], 2))
+    --    , ("(,)"      , ( ["(,)"], 2 ))
+    --    , ("Foo"      , ( ["Foo"], 2 ))
+    --    , ("#"        , ( ["#"], 1 ))
+    --    , ("{}"       , ( ["{}"], 0 ))
+    --    , ("Cons'"    , ( ["Nil'", "Cons'"], 3 ))
+    --    , ("Nil'"     , ( ["Nil'", "Cons'"], 0 ))
     ]
 
 runtest :: [[Pattern t]] -> Bool
@@ -38,58 +38,58 @@ test1 =
 -- exhaustive
 test2 :: [[Pattern ()]]
 test2 =
-  [ [ pLit () (IBool True) ]
-  , [ pLit () (IBool False) ]
+  [ [pLit () (IBool True)]
+  , [pLit () (IBool False)]
   ]
 
 -- not exhaustive
 test3 :: [[Pattern ()]]
 test3 =
-  [ [ pLit () (IBool True) ]
+  [ [pLit () (IBool True)]
   ]
 
 -- exhaustive
 test4 :: [[Pattern ()]]
 test4 =
   -- x :: (y :: ys)
-  [ [ pCon () "(::)" [ pVar () "x", pCon () "(::)" [ pVar () "y", pVar () "ys" ] ] ]
-  -- []
-  , [ pCon () "[]" [] ]
-  -- z :: zs
-  , [ pCon () "(::)" [ pVar () "z", pVar () "zs" ] ]
+  [ [pCon () "(::)" [pVar () "x", pCon () "(::)" [pVar () "y", pVar () "ys"]]]
+  , -- []
+    [pCon () "[]" []]
+  , -- z :: zs
+    [pCon () "(::)" [pVar () "z", pVar () "zs"]]
   ]
 
 -- not exhaustive
 test5 :: [[Pattern ()]]
 test5 =
   -- x :: (y :: ys)
-  [ [ pCon () "(::)" [ pVar () "x", pCon () "(::)" [ pVar () "y", pVar () "ys" ] ] ]
-  -- z :: zs
-  , [ pCon () "(::)" [ pVar () "z", pVar () "zs" ] ]
+  [ [pCon () "(::)" [pVar () "x", pCon () "(::)" [pVar () "y", pVar () "ys"]]]
+  , -- z :: zs
+    [pCon () "(::)" [pVar () "z", pVar () "zs"]]
   ]
 
 -- not exhaustive
 test6 :: [[Pattern ()]]
 test6 =
   -- x :: (y :: ys)
-  [ [ pCon () "(::)" [ pVar () "x", pCon () "(::)" [ pVar () "y", pVar () "ys" ] ] ]
-  -- z :: zs
-  , [ pCon () "(::)" [ pVar () "z", pVar () "zs" ] ]
-  -- _ :: _
-  , [ pCon () "(::)" [ pAny (), pAny () ] ]
+  [ [pCon () "(::)" [pVar () "x", pCon () "(::)" [pVar () "y", pVar () "ys"]]]
+  , -- z :: zs
+    [pCon () "(::)" [pVar () "z", pVar () "zs"]]
+  , -- _ :: _
+    [pCon () "(::)" [pAny (), pAny ()]]
   ]
 
 -- exhaustive
 test7 :: [[Pattern ()]]
 test7 =
   -- x :: (y :: ys)
-  [ [ pCon () "(::)" [ pVar () "x", pCon () "(::)" [ pVar () "y", pVar () "ys" ] ] ]
-  -- z :: zs
-  , [ pCon () "(::)" [ pVar () "z", pVar () "zs" ] ]
-  -- _ :: _
-  , [ pCon () "(::)" [ pAny (), pAny () ] ]
-  -- []
-  , [ pCon () "[]" [] ]
+  [ [pCon () "(::)" [pVar () "x", pCon () "(::)" [pVar () "y", pVar () "ys"]]]
+  , -- z :: zs
+    [pCon () "(::)" [pVar () "z", pVar () "zs"]]
+  , -- _ :: _
+    [pCon () "(::)" [pAny (), pAny ()]]
+  , -- []
+    [pCon () "[]" []]
   ]
 
 -- exhaustive
@@ -98,9 +98,9 @@ test8 =
   -- x :: y :: ys
   -- []
   -- z :: []
-  [ [ pCon () "(::)" [ pVar () "x", pCon () "(::)" [ pVar () "y", pVar () "ys" ] ] ]
-  , [ pCon () "[]" [] ]
-  , [ pCon () "(::)" [ pVar () "z", pCon () "[]" [] ] ]
+  [ [pCon () "(::)" [pVar () "x", pCon () "(::)" [pVar () "y", pVar () "ys"]]]
+  , [pCon () "[]" []]
+  , [pCon () "(::)" [pVar () "z", pCon () "[]" []]]
   ]
 
 -- not exhaustive
@@ -108,22 +108,22 @@ test9 :: [[Pattern ()]]
 test9 =
   -- x :: (y :: ys)
   -- []
-  [ [ pCon () "(::)" [ pVar () "x", pCon () "(::)" [ pVar () "y", pVar () "ys" ] ] ]
-  , [ pCon () "[]" [] ]
+  [ [pCon () "(::)" [pVar () "x", pCon () "(::)" [pVar () "y", pVar () "ys"]]]
+  , [pCon () "[]" []]
   ]
 
 -- not exhaustive
 test10 :: [[Pattern ()]]
 test10 =
   -- []
-  [ [ pCon () "[]" [] ]
+  [ [pCon () "[]" []]
   ]
 
 -- exhaustive
 test11 :: [[Pattern ()]]
 test11 =
   -- []
-  [ [ pAny () ]
+  [ [pAny ()]
   ]
 
 -- exhaustive
@@ -131,8 +131,8 @@ test12 :: [[Pattern ()]]
 test12 =
   -- x :: ys
   -- []
-  [ [ pCon () "(::)" [ pVar () "x", pVar () "ys" ] ] 
-  , [ pCon () "[]" [] ]
+  [ [pCon () "(::)" [pVar () "x", pVar () "ys"]]
+  , [pCon () "[]" []]
   ]
 
 -- exhaustive
@@ -140,8 +140,8 @@ test13 :: [[Pattern ()]]
 test13 =
   -- x :: ys
   -- x
-  [ [ pCon () "(::)" [ pVar () "x", pVar () "ys" ] ] 
-  , [ pVar () "x" ]
+  [ [pCon () "(::)" [pVar () "x", pVar () "ys"]]
+  , [pVar () "x"]
   ]
 
 -- exhaustive
@@ -149,8 +149,8 @@ test14 :: [[Pattern ()]]
 test14 =
   -- 5
   -- x
-  [ [ pLit () (IInt 5) ]  
-  , [ pVar () "x" ] 
+  [ [pLit () (IInt 5)]
+  , [pVar () "x"]
   ]
 
 -- not exhaustive
@@ -158,8 +158,8 @@ test15 :: [[Pattern ()]]
 test15 =
   -- 5
   -- x
-  [ [ pLit () (IInt 5) ]  
-  , [ pLit () (IInt 4) ] 
+  [ [pLit () (IInt 5)]
+  , [pLit () (IInt 4)]
   ]
 
 -- exhaustive
@@ -167,8 +167,8 @@ test16 :: [[Pattern ()]]
 test16 =
   -- 5, 5
   -- x, y
-  [ [ pLit () (IInt 5), pLit () (IInt 5) ]  
-  , [ pVar () "x", pVar () "y" ]
+  [ [pLit () (IInt 5), pLit () (IInt 5)]
+  , [pVar () "x", pVar () "y"]
   ]
 
 -- not exhaustive
@@ -176,14 +176,9 @@ test17 :: [[Pattern ()]]
 test17 =
   -- 5, 5
   -- x, 0
-  [ [ pLit () (IInt 5), pLit () (IInt 5) ]  
-  , [ pVar () "x", pLit () (IInt 0) ]
+  [ [pLit () (IInt 5), pLit () (IInt 5)]
+  , [pVar () "x", pLit () (IInt 0)]
   ]
-
-
-
-
-
 
 main :: IO ()
 main = print ("X" :: String) -- print kTyp
