78941fcd470fc28b1cb17d689760124d49682231
laserpants <hildenjohannes@gmail.com>
2022-08-31 04:11:03 +0300

    refactor

--- pong/test/Pong/UtilTests.hs
+++ pong/test/Pong/UtilTests.hs
@@ -2,7 +2,7 @@
 
 module Pong.UtilTests where
 
-import Control.Monad.State (put, runState)
+import Control.Monad.State (State, put, runState)
 import Pong.Util
 import Test.Hspec
 
@@ -10,35 +10,77 @@ utilTests :: SpecWith ()
 utilTests =
   describe "Pong.Util" $ do
     describe "- without" $ do
-      -------------------------------------------------------------------------
-      it "1" ([1, 2, 3, 4, 5] `without` [2 :: Int, 4, 6] == [1, 3, 5])
-      -------------------------------------------------------------------------
-      it "2" (null $ [] `without` [2 :: Int, 4, 6])
+      it "[1, 2, 3, 4, 5] `without` [2, 4, 6]" $
+        let input, result :: [Int]
+            input =
+              [1, 2, 3, 4, 5]
+            result =
+              [1, 3, 5]
+         in (input `without` [2, 4, 6] == result)
+      it "[1, 2, 3, 4, 5] `without` []" $
+        let input, result :: [Int]
+            input =
+              [1, 2, 3, 4, 5]
+            result =
+              [1, 2, 3, 4, 5]
+         in (input `without` [] == result)
+      it "[1, 2, 3, 4, 5] `without` [1, 1, 1]" $
+        let input, result :: [Int]
+            input =
+              [1, 2, 3, 4, 5]
+            result =
+              [2, 3, 4, 5]
+         in (input `without` [1, 1, 1] == result)
+      it "[1, 1, 1, 1, 2, 4] `without` [1, 2, 3]" $
+        let input, result :: [Int]
+            input =
+              [1, 1, 1, 1, 2, 4]
+            result =
+              [4]
+         in (input `without` [1, 2, 3] == result)
+      it "[] `without` [2, 4, 6]" $
+        let input, result :: [Int]
+            input =
+              []
+            result =
+              []
+         in (input `without` [2, 4, 6] == result)
 
-    describe "- getAndModify" $ do
-      -------------------------------------------------------------------------
-      let prog = do
-            put 1
-            getAndModify (+ 1)
+    describe "- getAndModify" $
+      it "put 1 >> getAndModify succ" $
+        let sample :: State Int Int
+            sample = do
+              put 1
+              getAndModify succ
+            result :: (Int, Int)
+            result =
+              (1, 2)
+         in (runState sample 0 == result)
 
-      it "1" (runState prog 0 == (1 :: Int, 2))
-
-    describe "- varSequence" $ do
-      -------------------------------------------------------------------------
-      it "1" $
-        let result =
+    describe "- varSequence" $
+      it "varSequence \"foo\" [0, 1, 2]" $
+        let input :: [Int]
+            input =
+              [0, 1, 2]
+            result :: [(Int, Text)]
+            result =
               [ (0, "foo0")
               , (1, "foo1")
               , (2, "foo2")
               ]
-         in varSequence "foo" [0 :: Int, 1, 2] == result
+         in varSequence "foo" input == result
 
-    describe "- (<$$>)" $ do
-      -------------------------------------------------------------------------
-      it "1" $
-        let result =
+    describe "- (<$$>)" $
+      it "succ <$$> [((), 1), ((), 2), ((), 42)]" $
+        let input, result :: [((), Int)]
+            input =
+              [ ((), 1)
+              , ((), 2)
+              , ((), 42)
+              ]
+            result =
               [ ((), 2)
               , ((), 3)
               , ((), 43)
               ]
-         in (((+ 1) <$$> [((), 1 :: Int), ((), 2), ((), 42)]) == result)
+         in ((succ <$$> input) == result)
